8a11912b8fead0d91a778c3fc4389762c44a8cb0
riveur <riveur.cpy@gmail.com>
2023-07-30 14:31:33 +0400

    setup shadcn/ui

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function classNames(...classes: ClassValue[]) 
-{
-    return twMerge(clsx(classes));
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
